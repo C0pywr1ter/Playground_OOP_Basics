062950a452aec8ee61bf1c3795bc4f3cac1356e0
Nikita <nikitag2709@gmail.com>
2025-10-21 15:47:43 +0200

    book class added, instance of book in main added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,23 @@
+import java.util.Date;
 public class Main {
+    void main(){
+        Book mmm = new Book();
+        mmm.author = "Fred Brooks";
+        mmm.title = "The mythical man";
+        mmm.edition = 2;
+    }
 
+}
+class Book {
+    String author;
+    String title;
+    int edition;
+
+    void Borrow(Date s, Date e){
+
+    }
+
+    void GiveBack(Date today){
+
+    }
 }
